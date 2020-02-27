4f18705a16750b7293ebb89d36265a6a5c919fd3
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-27 09:50:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -91,7 +91,7 @@ class Dog
 
   def self.find_by_name(name)
     sql = <<-SQL
-    SELECT*FROM dogs WHERE dogs.name = ?
+    SELECT * FROM dogs WHERE dogs.name = ?
     SQL
     DB[:conn].execute(sql, name)
   end

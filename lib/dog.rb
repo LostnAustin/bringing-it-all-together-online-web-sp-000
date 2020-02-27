149f0e72e9e25f8f4fcdd43f6694764b05143851
Chad Lengefeld <chadlengefeld@gmail.com>
2020-02-27 06:41:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,8 +50,8 @@ class Dog
 
   def self.create(hash)
     # binding.pry
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    dog = Dog.new(id: @id,  name: self.name, breed: self.breed)
+    # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    dog = Dog.new(name: self.name, breed: self.breed)
 
     dog
   end
